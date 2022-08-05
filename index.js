fb10b324b69cd7049d481dbc8d146515079b0443
Asagba Mubarak Anthony <hembee999@gmail.com>
2022-08-05 02:56:51 +0100

    h

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-let button= document.getElementsByTagName('button');
-button.addEventListener('click', function(){
-    alert('clicked')
-})
+
+
+var msg= document.getElementById('msg')
+var img1= document.getElementsByClassName('img1');
+var img2= document.getElementsByClassName('img2');
+
+function game(){
+ var random= Math.ceil(Math.random() * 6);
+ var random2= Math.ceil(Math.random() * 6);
+ img1.src = `images/dice${random}.png`;
+ img2.src = `images/dice${random2}.png`;
+
+
+    if(random > random2){
+        msg.innerHTML= 'Player 1 WIN!!'
+    }else{
+        msg.innerHTML= 'Player 2 WIN!!'
+    }
+}
